0309405f3d0e97e9c65863a8ff596abc8d42dac0
Rajesh <srajesh@paypalcorp.com>
2016-08-09 14:19:17 +0530

    hand

--- src/Cards/Card.java
+++ src/Cards/Card.java
@@ -1,5 +1,9 @@
 package Cards;
 
 public class Card {
+	
+	public int compare(Card a, Card b){
+		return 0;
+	}
 
 }
--- src/Cards/Hand.java
+++ src/Cards/Hand.java
@@ -1,5 +1,43 @@
 package Cards;
 
-public class Hand {
+import java.util.List;
 
+public class Hand {
+	private List<Card> cards;
+	
+	public Hand(){
+	}
+	/**
+	 * Adds a card to the Hand
+	 * @param num Position of the card.
+	 * @return returns a success
+	 */
+	public boolean addCard(Card c){	
+		return cards.add(c);
+	}
+	
+	/**
+	 * removes a card from the Hand
+	 * @param num Position of the card.
+	 * @return returns a success
+	 */
+	public boolean removeCard(Card c){
+		return cards.remove(c);
+	}
+	
+	/**
+	 * sorts the cards in the hand
+	 * @return returns the shuffled list of cards
+	 */
+	public List<Card> sort(){
+		return null;
+	}
+	
+	/*
+	 *  @return returns the card in the hand
+	 */
+	public List<Card> cardsInHand(){
+		return cards;;
+	}
+	
 }
